2a2cb642f4699be3b3770956673f2c8d4791066d
Алексей Ширшов <proplayer913@gmail.com>
2025-09-11 13:45:42 +0300

    Task-0004 Добавлен мок тест

--- Tests/DataHarvesterTabletkaBy/Strategies/DataHarvesterStrategyTests.cs
+++ Tests/DataHarvesterTabletkaBy/Strategies/DataHarvesterStrategyTests.cs
@@ -3,6 +3,7 @@ using System.Net.Http;
 using DataHarvester.Strategies;
 using DataHarvesterTabletkaBy.Strategies;
 using Moq;
+using Moq.Protected;
 
 namespace Tests.DataHarvesterTabletkaBy.Strategies
 {
@@ -20,7 +21,50 @@ namespace Tests.DataHarvesterTabletkaBy.Strategies
 
 		public DataHarvesterStrategyTests()
 		{
+			_httpMessageHandlerMock = new Mock<HttpMessageHandler>();
 
+			var list = File.ReadAllText(Path.Combine(FilesDirectory, "drug-list.html"));
+			var drug = File.ReadAllText(Path.Combine(FilesDirectory, "drug.html"));
+
+			var listResponse = new HttpResponseMessage
+			{
+				StatusCode = HttpStatusCode.OK,
+				Content = new StringContent(list)
+			};
+
+			var drugResponse = new HttpResponseMessage
+			{
+				StatusCode = HttpStatusCode.OK,
+				Content = new StringContent(drug)
+			};
+
+			// Первый запрос - Список лекарств
+			_httpMessageHandlerMock.Protected()
+				.Setup<Task<HttpResponseMessage>>(
+					"SendAsync",
+					ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.ToString().Contains("/drugs/")),
+					ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(listResponse);
+
+			// Второй запрос - Страница лекарства
+			_httpMessageHandlerMock.Protected()
+				.Setup<Task<HttpResponseMessage>>(
+					"SendAsync",
+					ItExpr.Is<HttpRequestMessage>(req => req.RequestUri.ToString().Contains("/search/")),
+					ItExpr.IsAny<CancellationToken>())
+				.ReturnsAsync(drugResponse);
+
+			_httpClient = new HttpClient(_httpMessageHandlerMock.Object);
+			_strategy = new DataHarvesterStrategy(_httpClient);
+		}
+
+		[TestMethod]
+		public async Task GetDrugsByLetterAsync_Success()
+		{
+			var drus = await _strategy.GetDrugsByLetterAsync("А");
+
+			Assert.IsNotNull(drus);
+			Assert.AreEqual(3, drus.Count);
 		}
 
 		[TestMethod]
